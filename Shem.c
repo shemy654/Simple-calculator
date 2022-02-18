f84065c1a934bc93340361cab06571bf32e72a91
shemy654 <62020607+shemy654@users.noreply.github.com>
2022-02-18 13:01:36 +0300

    Add files via upload

---
+++ Shem.c
@@ -0,0 +1,39 @@
+/*Simple calculator project
+By Shem nyaga
+Last updated on fifth 15th Feb, 2022
+C89 compiler
+MIT LICENSE
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	//variable declaration
+	char name [100];
+	int n1,n2,sum,product,diff;
+	float quot;
+	printf("Simple calculator!\n");
+	
+	//input
+	printf("Enter your name:");
+	gets(name);
+	printf("Enter two integers;");
+	scanf("%d%d",&n1,&n2);
+	
+	//computations
+	sum= n1 + n2;
+	diff= n1 - n2;
+	product= n1* n2;
+	quot= (float) n1/n2;
+	
+	//output
+	printf("Hey %s,here are the results: \n",name);
+	printf("%d+%d=%d",n1,n2,sum);
+	printf("%d+%d=%d",n1,n2,diff);
+	printf("%d+%d=%d",n1,n2,product);
+	printf("%d+%d=%d",n1,n2,quot);
+	return 0;
+}
+
+
